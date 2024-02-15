385d4ed60389459f180955c6fb9b95c2d17d8714
Maglys Maitae <mmaita83@gmail.com>
2024-02-14 22:19:05 -0300

    Se realiza la Funcion guardar tarea

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+//Funcion guardar Tarea
+ const guardarTarea = async(tarea)=>{
+    const consulta = "INSERT INTO tareas values (DEFAULT,$1)";
+    const values= [tarea];
+    const result = await Pool.query(consulta,values)
+}
+
 
 
 app.listen(3000, console.log('Server ON'))
